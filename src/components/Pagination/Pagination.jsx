9fcc4a8b07cdfa732f6f6a12cace1b24d5af010e
Sergeevu4 <sergei.sergeevu4@gmail.com>
2020-04-23 14:54:43 +0300

    Pagination: Update Layout and adding Handler to switch pages

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -1,37 +1,68 @@
 import React from 'react';
+import { getPaginationNumbers } from './getPaginationNumbers';
+import classNames from 'classnames';
 
-const Pagination = ({ page, onPaginateNext, onPaginatePrev }) => {
-  const nextPage = () => (evt) => {
+const Pagination = ({
+  currentPage,
+  totalPages,
+  onPaginateNumber,
+  onPaginateNext,
+  onPaginatePrev,
+  onPaginateFirst,
+  onPaginateLast,
+}) => {
+  const maxPages = 10;
+  const pageNumbers = getPaginationNumbers(currentPage, totalPages, maxPages);
+
+  const handelClick = (fn, ...args) => (evt) => {
     evt.preventDefault();
-    onPaginateNext();
+    fn(...args);
   };
 
-  const prevPage = () => (evt) => {
-    evt.preventDefault();
-    onPaginatePrev();
-  };
+  const getClass = (page, currentPage, name) =>
+    classNames('page-item', {
+      [name]: page === currentPage,
+    });
 
   return (
     <nav>
       <ul className='pagination d-flex justify-content-center'>
-        <li className={`page-item`}>
-          <a href='!#' className='page-link' onClick={prevPage()}>
-            Previous
+        <li className='page-item'>
+          <a href='!#' className='page-link' onClick={handelClick(onPaginateFirst)}>
+            First
           </a>
         </li>
 
-        <li className={`page-item`}>
-          <a href='!#' className='page-link'>
-            {page}
+        <li className={getClass(1, currentPage, 'disabled')}>
+          <a href='!#' className='page-link' onClick={handelClick(onPaginatePrev)}>
+            <span aria-hidden='true'>&laquo;</span>
           </a>
         </li>
+
+        {pageNumbers.map((pageNumber) => (
+          <li key={pageNumber} className={getClass(pageNumber, currentPage, 'active')}>
+            <a
+              href='!#'
+              className='page-link'
+              onClick={handelClick(onPaginateNumber, pageNumber)}
+            >
+              {pageNumber}
+            </a>
+          </li>
+        ))}
 
         <li
           // Отключить если последняя стараница
-          className={`page-item`}
+          className={getClass(totalPages, currentPage, 'disabled')}
         >
-          <a href='!#' className='page-link' onClick={nextPage()}>
-            Next
+          <a href='!#' className='page-link' onClick={handelClick(onPaginateNext)}>
+            <span aria-hidden='true'>&raquo;</span>
+          </a>
+        </li>
+
+        <li className='page-item'>
+          <a href='!#' className='page-link' onClick={handelClick(onPaginateLast)}>
+            Last
           </a>
         </li>
       </ul>
