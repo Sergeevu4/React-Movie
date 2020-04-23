95dbff0afbb1d4570b5536e093caee15a046d813
Sergeevu4 <sergei.sergeevu4@gmail.com>
2020-04-23 19:28:29 +0300

    index.js: re-export of components and containers

---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+export { App } from './App/App.jsx';
+export { Error } from './Error/Error.jsx';
+export { Spinner } from './Spinner/Spinner.jsx';
+export { MovieItem } from './MovieItem/MovieItem.jsx';
+export { MovieList } from './MovieList/MovieList.jsx';
+export { MovieTabs } from './MovieTabs/MovieTabs.jsx';
+export { MovieWillWatch } from './MovieWillWatch/MovieWillWatch.jsx';
+export { Pagination } from './Pagination/Pagination.jsx';
---
+++ src/containers/index.js
@@ -0,0 +1,4 @@
+export { default as MovieList } from './MovieListContainer/MovieListContainer';
+export { default as MovieTabs } from './MovieTabsContainer/MovieTabsContainer';
+export { default as MovieWillWatch } from './MovieWillWatchContainer/MovieWillWatchContainer';
+export { default as Pagination } from './PaginationContainer/PaginationContainer';
