c18be89d00cd26b23451d200cd082cf0e82067b5
Sergeevu4 <sergei.sergeevu4@gmail.com>
2020-04-26 00:05:34 +0300

    index.js: re-export Reducers

---
+++ src/reducers/index.js
@@ -0,0 +1,4 @@
+export { movieListReducer } from './movieListReducer';
+export { movieWillWatchListReducer } from './movieWillWatchListReducer';
+export { pageReducer } from './pageReducer';
+export { sortTypeByMovieReducer } from './sortTypeByMovieReducer';
