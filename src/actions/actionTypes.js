4d76d767965c8bdd96680a8ed92606b3dd62c192
Sergeevu4 <sergei.sergeevu4@gmail.com>
2020-04-26 00:15:45 +0300

    actionTypes: update

--- src/actions/actionTypes.js
+++ src/actions/actionTypes.js
@@ -5,14 +5,21 @@ export const FETCH_MOVIES_SUCCESS = 'FETCH_MOVIES_SUCCESS';
 // В момент загрузки произошла ошибка
 export const FETCH_MOVIES_FAILURE = 'FETCH_MOVIES_FAILURE';
 // Изменения метода серверной сортировки в момент отправки запроса
-export const FETCH_CHANGE_SORT_MOVIES = 'FETCH_CHANGE_SORT_MOVIES';
+export const FETCH_CHANGE_SORT_TYPE_BY_MOVIES = 'FETCH_CHANGE_SORT_TYPE_BY_MOVIES';
+// Удаление фильма из каталоге фильмов
+export const REMOVE_MOVIE = 'REMOVE_MOVIE';
+// Переключение свойства просмотра в каталоге все фильмов
+export const TOGGLE_PROPERTY_WILL_WATCH_MOVIE = 'TOGGLE_PROPERTY_WILL_WATCH_MOVIE';
+// Все фильмы withWatch - false
+export const RESET_PROPERTY_WILL_WATCH_MOVIE = 'RESET_PROPERTY_WILL_WATCH_MOVIE';
+
 // Установка выбранной страницы
 export const SET_CURRENT_PAGE = 'SET_CURRENT_PAGE';
-// Удаление фильма из перечня фильмов
-export const REMOVE_MOVIE = 'REMOVE_MOVIE';
 // Добавления фильма в список для просмотра
 export const ADD_MOVIE_WILL_WATCH = 'ADD_MOVIE_WILL_WATCH';
 // Удаления фильма из списка для просмотра
 export const REMOVE_MOVIE_WILL_WATCH = 'REMOVE_MOVIE_WILL_WATCH';
 // Удаления всех фильмов из списка для просмотра
 export const REMOVE_ALL_MOVIE_WILL_WATCH = 'REMOVE_ALL_MOVIE_WILL_WATCH';
+
+export const SET_TOTAL_PAGES = 'SET_TOTAL_PAGES';
